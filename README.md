57333352b5e9e449f3c18ab000fdebf2ff5b4bd9
Edmund Situmorang <edmundsitumorang@gmail.com>
2019-04-30 19:25:06 +0700

    using chromedriver for mac version 74

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # whatsapp-python-send-contacts
 This will automate sending messages to several contacts in WhatsApp using Python
+
+Step 1: Install Selenium 
+```sh
+$ pip3 install selenium
+```
+
+Step 2: Selenium requires a driver to interface with the chosen browser.
+[Chrome](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+[FireFox](https://github.com/mozilla/geckodriver/releases)
+[safari](https://webkit.org/blog/6900/webdriver-support-in-safari-10)
+
+Step 3: Extract the downloaded driver onto a folder
+
+Step 4: Set path variable to the environment. Paste this command to the terminal
+```sh
+$ export PATH=$PATH:/home/pathofthedriverfolder/
+Eg: $ export PATH=$PATH:/home/situmorang/Download/WhatsappSend
+```
+Step 5: run whatsapp.py using Python3
+```sh
+$ python3 whatsapp.py
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome(executable_path='./chromedriver/chromedriver')
+driver = webdriver.Chrome('./chromedriver/chromedriver_mac64_74')
 driver.get('https://web.whatsapp.com/')
 
 # name = input('Enter the name of user or group : ')
-name = "situmorang.com"
+name_list = ['Edmund Situmorang', 'Mom']
+
 msg = input('Enter your message : ')
 count = int(input('Enter the count : '))
 
 input('Enter anything after scanning QR code')
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
+for name in name_list:
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
 
-msg_box = driver.find_element_by_class_name('_2S1VP')
+    msg_box = driver.find_element_by_class_name('_2S1VP')
 
-for i in range(count):
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_35EW6')
-    button.click()
+    for i in range(count):
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_class_name('_35EW6')
+        button.click()
